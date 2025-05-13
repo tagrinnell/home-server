785d11e080e829384811e9912c9f6021f2726261
tagrinnell <tasman.grinnell@gmail.com>
2025-05-13 12:58:02 -0500

    More Edits / Cleaning up the repo

--- create_dirs.sh
+++ create_dirs.sh
@@ -4,12 +4,14 @@
 # Helper Script to create all the docker volumes needed for configuration
 # Based in the /mnt folder
 #
+# Note: Needs to be run as Sudo to create folders in /mnt/
+#
 
 cd /mnt
 # Create Config folders
 mkdir -p vol/dockerdata/lib/modules
 cd vol/dockerdata
-mkdir plex sonarr radarr qbt prowlarr overseerr wireguard
+mkdir -p  plex sonarr radarr qbt prowlarr overseerr jelly/config jelly/cache
 # Create Media folders
 cd ..
-mkdir -p photo music video/tv video/movies video/downloads video/downloadsMovies
+mkdir -p photo music video/tv video/movies video/Downloads
--- docs/SettingUpPlex.md
+++ docs/SettingUpPlex.md
@@ -11,3 +11,5 @@ This is a document listing the steps that I took to get the plex ecosystem up an
         1. TODO: Figure out if/how to set up a language profile for sonarr --> Overseerr requires a language profile, but the only one available is 'Deprecated'
 5. Set up Prowlarr
     1. Register Sonarr and Radarr 
+6. Add QbitTorrent as download clients to Radarr and Sonarr
+7. Add libraries for Jellyfin and Plex
